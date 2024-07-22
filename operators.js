e869ef3b0b09e86eaf52c6c7983957d8efc43639
Bolarinwa Ahmed <bolarinwaahmed22@gmail.com>
2024-07-22 10:09:15 +0100

    Basics and Operator class

---
+++ operators.js
@@ -0,0 +1,110 @@
+// console.log("Welcome to learning operators!!!");
+// // Types of Operators
+// // 1. Aritemetic Operators
+// // 2. Assignment Operators
+// // 3. Comparison Operators
+// // 4. Logical Operators
+
+// // 1. ARITHMETIC OPERATORS
+// let numberOne = 7;
+// let numberTwo = 3;
+
+// // a. Addition Operator
+// let sum = null;
+// sum = numberOne + numberTwo;
+
+// console.log(numberOne);
+// console.log(numberTwo);
+// console.log(sum);
+// console.log(typeof sum);
+
+// // b. Subtraction
+// let subtract = numberTwo - numberOne;
+// console.log(subtract);
+
+// // c. Multiplication
+// let multiply = numberOne * numberTwo;
+// console.log(multiply);
+
+// // d. Division
+// let divide = numberOne / numberTwo;
+// console.log(divide);
+
+// // e. Remainder (%)
+// let remainder = numberOne % numberTwo;
+// console.log(remainder);
+
+// // f. Exponential (**)
+// let exponential = numberOne ** numberTwo;
+// console.log(exponential);
+
+// g. Increment Operator
+// let initialNumber = 5;
+// console.log(initialNumber);
+// initialNumber++;
+// let increment;
+// console.log(initialNumber);
+// increment = initialNumber;
+// console.log(increment); //
+// console.log(initialNumber++);
+// console.log(initialNumber);
+// increment = initialNumber++;
+
+// h. Decrement Operator
+// let firstNumber = 10;
+// console.log(firstNumber);
+// let test = --firstNumber;
+// console.log(test);
+
+// Assignment Operator
+// let x = 6;
+// let y = 5;
+// // x = x + y;
+// x **= y;
+// console.log(x);
+
+// Comparison Operators
+// let a = 10;
+// let b = 15;
+// console.log(a);
+// console.log(b);
+// // Equal to
+// console.log("Equals to");
+// console.log(a == b);
+
+// // Not Equals to
+// console.log("Not Equals to");
+// console.log(a != b);
+
+// // Greater than
+// console.log("Greater than");
+// console.log(a > b);
+
+// // Less than
+// console.log("Less than");
+// console.log(a < b);
+
+// // Greater than OR Equals to
+// console.log("Greater than or equal to");
+// console.log(a >= b);
+
+// // Less than OR Equals to
+// console.log("Less than or equal to");
+// console.log(a <= b);
+
+// Logical Operator
+// OR
+// AND
+// NOT
+
+let isMarried = true;
+let isRichYet = true;
+
+// And
+console.log(isMarried && isRichYet);
+
+// Or
+console.log(isMarried || isRichYet);
+
+// NOT
+console.log(!isRichYet);
---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Hello World!!!");
+console.log("I am enjoying this i think!");
+console.log(2 + 2);
+console.log(10 - 4);
+console.log("2" + "2");
+
+// alert("Hello World!!!");
+// alert("I am enjoying this i think!");
+
+// Types of comments in JS
+// Single Line Comment
+/*
+  Multi Line Comment
+hfbvhudihude
+ygfuehwuf
+yuegduhweuduwgeygfey
+*/
+
+// Variables
+// var, let, const
+
+let fullName = "John Doe";
+let age = 30;
+const gender = "male";
+var isMarried = true;
+var sentence = "Your name is: " + fullName + "And I really like you";
+console.log(sentence);
+console.log(fullName + "'s age is " + age);
+console.log(fullName + " is a ", gender);
+console.log(fullName + " is married: " + isMarried);
+
+console.log(fullName);
+fullName = "Ajanaku Agbabiaka";
+console.log(fullName);
+
+// Data Types
+// Primitive Data Types
+//  String
+// Numbers
+// Boolean
+
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof isMarried);
